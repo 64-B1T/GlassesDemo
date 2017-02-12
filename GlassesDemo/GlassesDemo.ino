b5a832afa10a0cddcd2158d0ed28761267914b41
64-B1T <liamchapin.com@gmail.com>
2017-02-12 09:33:28 -0500

    Add files via upload

---
+++ GlassesDemo/GlassesDemo.ino
@@ -0,0 +1,236 @@
+#include <Wire.h>  // Include Wire if you're using I2C
+#include <SPI.h>  // Include SPI if you're using SPI
+#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+
+//////////////////////////
+// MicroOLED Definition //
+//////////////////////////
+#define PIN_RESET 9  // Connect RST to pin 9
+#define PIN_DC    8  // Connect DC to pin 8
+#define PIN_CS    10 // Connect CS to pin 10
+#define DC_JUMPER 0
+
+
+float d = 3;
+float px[] = { 
+  -d,  d,  d, -d, -d,  d,  d, -d };
+float py[] = { 
+  -d, -d,  d,  d, -d, -d,  d,  d };
+float pz[] = { 
+  -d, -d, -d, -d,  d,  d,  d,  d };
+
+float p2x[] = {
+  0,0,0,0,0,0,0,0};
+float p2y[] = {
+  0,0,0,0,0,0,0,0};
+
+float r[] = {
+  0,0,0};
+
+#define SHAPE_SIZE 600
+// Define how fast the cube rotates. Smaller numbers are faster.
+// This is the number of ms between draws.
+#define ROTATION_SPEED 0
+
+/////////////////////////////////////////////////////////////////
+uint8_t bender [] = {
+0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x01, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x07, 0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+0x07, 0x07, 0x07, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x07, 0x07, 0x07, 0x07,
+0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFF, 0xFF, 0xFF, 0xC1, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x10, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0xE0, 0xE0, 0xF0, 0xF0, 0xF0, 0xF8, 0xF8,
+0xFC, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE,
+0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x3F, 0x1C,
+0x1C, 0x08, 0x08, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x07, 0x07, 0x0F, 0x0F, 0x0F, 0x1F,
+0x1F, 0x3F, 0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0xE0, 0xC0,
+0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x80,
+0xC0, 0xC0, 0xE0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+};
+
+
+
+//////////////////////////////////
+// MicroOLED Object Declaration //
+//////////////////////////////////
+MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS); // SPI declaration
+//MicroOLED oled(PIN_RESET, DC_JUMPER);    // I2C declaration
+boolean flip = true;
+
+void setup()
+{
+  
+  oled.begin();    // Initialize the OLED
+  oled.flipHorizontal(flip);
+  oled.flipVertical(flip);
+  oled.setRotation(0);
+  oled.clear(ALL); // Clear the display's internal memory
+  oled.display();// Display what's in the buffer (splashscreen)
+ 
+  delay(1000);     // Delay 1000 ms
+  oled.clear(PAGE); // Clear the buffer.
+  
+  randomSeed(analogRead(A0) + analogRead(A1));
+
+  int middleX = oled.getLCDWidth() / 2;
+  int middleY = oled.getLCDHeight() / 2;
+  int SCREEN_WIDTH = oled.getLCDWidth();
+int SCREEN_HEIGHT = oled.getLCDHeight();
+  
+  int xEnd, yEnd;
+  int lineWidth = min(middleX, middleY);
+  
+  printTitle("Booting", 1);
+  
+  for (int i=0; i<3; i++)
+  {
+    for (int deg=0; deg<360; deg+=15)
+    {
+      xEnd = lineWidth * cos(deg * PI / 180.0);
+      yEnd = lineWidth * sin(deg * PI / 180.0);
+      
+      oled.line(middleX, middleY, middleX + xEnd, middleY + yEnd);
+      oled.display();
+      delay(10);
+    }
+    for (int deg=0; deg<360; deg+=15)
+    {
+      xEnd = lineWidth * cos(deg * PI / 180.0);
+      yEnd = lineWidth * sin(deg * PI / 180.0);
+      
+      oled.line(middleX, middleY, middleX + xEnd, middleY + yEnd, BLACK, NORM);
+      oled.display();
+      delay(10);
+    }
+  }
+  oled.clear(ALL);
+  printTitle("Frugal", 1);
+  delay(20);
+  oled.clear(ALL);
+  printTitle("Glass", 1);
+  delay(20);
+}
+
+void boot(){
+  ///printTitle("Text!", 1);
+  
+  // Demonstrate font 0. 5x8 font
+  oled.clear(PAGE);     // Clear the screen
+  oled.setFontType(0);  // Set font to type 0
+  oled.setCursor(0, 0); // Set cursor to top-le
+  
+  // Demonstrate font 1. 8x16. Let's use the print function
+  // to display every character defined in this font.
+  oled.setFontType(1);  // Set font to type 1
+  oled.clear(PAGE);     // Clear the page
+  oled.setCursor(0, 0); // Set cursor to top-left
+  // Print can be used to print a string to the screen:
+  oled.print(" Read SMS Messages!");
+  oled.display();       // Refresh the display
+  delay(1000);          // Delay a second and repeat
+  oled.clear(PAGE);
+  oled.setCursor(0, 0);
+  oled.print("From:   Mom");
+  oled.display();
+  delay(1000);
+  oled.clear(PAGE);
+  oled.setCursor(0, 0);
+  oled.print("Did you sleep at all?");
+  oled.display();
+  delay(1000);
+  oled.clear(PAGE);
+  oled.setCursor(0, 0);
+  oled.print("    ...     ");
+  oled.display();
+  delay(1000);
+  oled.clear(PAGE);
+  oled.setCursor(0, 0);
+  oled.print("  <No.>   ");
+  oled.display();
+  delay(1000);
+  oled.clear(PAGE);
+}
+
+void cube(){
+  int SCREEN_WIDTH = oled.getLCDWidth();
+  int SCREEN_HEIGHT = oled.getLCDHeight();
+
+   r[0]=r[0]+PI/180.0; // Add a degree
+  r[1]=r[1]+PI/180.0; // Add a degree
+  r[2]=r[2]+PI/180.0; // Add a degree
+  if (r[0] >= 360.0*PI/180.0) r[0] = 0;
+  if (r[1] >= 360.0*PI/180.0) r[1] = 0;
+  if (r[2] >= 360.0*PI/180.0) r[2] = 0;
+
+  for (int i=0;i<4;i++)
+  {
+    float px2 = px[i];
+    float py2 = cos(r[0])*py[i] - sin(r[0])*pz[i];
+    float pz2 = sin(r[0])*py[i] + cos(r[0])*pz[i];
+
+    float px3 = cos(r[1])*px2 + sin(r[1])*pz2;
+    float py3 = py2;
+    float pz3 = -sin(r[1])*px2 + cos(r[1])*pz2;
+
+    float ax = cos(r[2])*px3 - sin(r[2])*py3;
+    float ay = sin(r[2])*px3 + cos(r[2])*py3;
+    float az = pz3-150;
+
+    p2x[i] = SCREEN_WIDTH/2+ax*SHAPE_SIZE/az;
+    p2y[i] = SCREEN_HEIGHT/2+ay*SHAPE_SIZE/az;
+  }
+
+  oled.clear(PAGE);
+  for (int i=0;i<3;i++) 
+  {
+    oled.line(p2x[i],p2y[i],p2x[i+1],p2y[i+1]);
+    oled.line(p2x[i+4],p2y[i+4],p2x[i+5],p2y[i+5]);
+    oled.line(p2x[i],p2y[i],p2x[i+4],p2y[i+4]);
+  }    
+  oled.line(p2x[3],p2y[3],p2x[0],p2y[0]);
+  oled.line(p2x[7],p2y[7],p2x[4],p2y[4]);
+  oled.line(p2x[3],p2y[3],p2x[7],p2y[7]);
+  oled.display();
+}
+
+void printTitle(String title, int font)
+{
+  int middleX = oled.getLCDWidth() / 2;
+  int middleY = oled.getLCDHeight() / 2;
+  
+  oled.clear(PAGE);
+  oled.setFontType(font);
+  // Try to set the cursor in the middle of the screen
+  oled.setCursor(middleX - (oled.getFontWidth() * (title.length()/2)),
+                 middleY - (oled.getFontWidth() / 2));
+  // Print the title:
+  oled.print(title);
+  oled.display();
+  delay(1500);
+  oled.clear(PAGE);
+}
+
+void mlh(){
+  oled.clear(PAGE);//clear the screen before we draw our image
+  oled.drawBitmap(bender);//call the drawBitmap function and pass it the array from above
+  oled.display();//display the imgage
+  delay(2000);
+}
+void loop() {
+  boot();
+  mlh();
+  cube();
+  
+}
